11432dfc0cd69f8b7d6ed550829e947cd5745de9
smallvi <smallvi@gmail.com>
2024-07-25 21:13:30 +0800

    feat: add arrow

--- yoyo-tooltip.js
+++ yoyo-tooltip.js
@@ -1,50 +1,94 @@
-/*! Yoyo Tooltip v1.0.0 (https://github.com/smallvi/yoyo_tooltip) */
+/*! Yoyo Tooltip v1.0.0 (https://github.com/smallvi/yoyo-tooltip) */
 function addTooltipStyles() {
   if (document.getElementById('yoyo-tooltip-styles')) return;
 
   const style = document.createElement('style');
   style.id = 'yoyo-tooltip-styles';
   style.textContent = `
-      .yoyo-tooltip {
-        position: absolute;
-        color: #fff;
-        padding: 5px 10px;
-        border-radius: 5px;
-        white-space: nowrap;
-        opacity: 0;
-        visibility: hidden;
-        transition: opacity 1s ease-in-out, visibility 1s ease-in-out;
-      }
-  
-      .yoyo-tooltip.top {
-        transform: translateX(-50%);
-        bottom: 125%;
-        left: 50%;
-      }
-  
-      .yoyo-tooltip.right {
-        transform: translateY(-50%);
-        top: 50%;
-        left: 125%;
-      }
-  
-      .yoyo-tooltip.bottom {
-        transform: translateX(-50%);
-        top: 125%;
-        left: 50%;
-      }
-  
-      .yoyo-tooltip.left {
-        transform: translateY(-50%);
-        top: 50%;
-        right: 125%;
-      }
-  
-      .yoyo-tooltip.fade-in {
-        opacity: 1;
-        visibility: visible;
-      }
-    `;
+    .yoyo-tooltip {
+      position: absolute;
+      color: #fff;
+      padding: 5px 10px;
+      border-radius: 5px;
+      white-space: nowrap;
+      opacity: 0;
+      visibility: hidden;
+      transition: opacity 1s ease-in-out, visibility 1s ease-in-out;
+    }
+
+    .yoyo-tooltip.top {
+      transform: translateX(-50%);
+      bottom: 125%;
+      left: 50%;
+    }
+
+    .yoyo-tooltip.top::after {
+      content: "";
+      position: absolute;
+      top: 100%;
+      left: 50%;
+      margin-left: -5px;
+      border-width: 5px;
+      border-style: solid;
+      border-color: var(--tooltip-bg-color) transparent transparent transparent;
+    }
+
+    .yoyo-tooltip.right {
+      transform: translateY(-50%);
+      top: 50%;
+      left: 125%;
+    }
+
+    .yoyo-tooltip.right::after {
+      content: "";
+      position: absolute;
+      top: 50%;
+      right: 100%;
+      margin-top: -5px;
+      border-width: 5px;
+      border-style: solid;
+      border-color: transparent var(--tooltip-bg-color) transparent transparent;
+    }
+
+    .yoyo-tooltip.bottom {
+      transform: translateX(-50%);
+      top: 125%;
+      left: 50%;
+    }
+
+    .yoyo-tooltip.bottom::after {
+      content: "";
+      position: absolute;
+      bottom: 100%;
+      left: 50%;
+      margin-left: -5px;
+      border-width: 5px;
+      border-style: solid;
+      border-color: transparent transparent var(--tooltip-bg-color) transparent;
+    }
+
+    .yoyo-tooltip.left {
+      transform: translateY(-50%);
+      top: 50%;
+      right: 125%;
+    }
+
+    .yoyo-tooltip.left::after {
+      content: "";
+      position: absolute;
+      top: 50%;
+      left: 100%;
+      margin-top: -5px;
+      border-width: 5px;
+      border-style: solid;
+      border-color: transparent transparent transparent var(--tooltip-bg-color);
+    }
+
+    .yoyo-tooltip.fade-in {
+      opacity: 1;
+      visibility: visible;
+    }
+  `;
   document.head.appendChild(style);
 }
 
@@ -52,7 +96,7 @@ function yoyoTooltip({ id, direction = 'top', content, delay = 0, trigger = 'hov
   addTooltipStyles();
 
   const element = document.querySelector(id);
-  
+
   if (!element) return;
 
   const tooltip = document.createElement('div');
@@ -60,6 +104,8 @@ function yoyoTooltip({ id, direction = 'top', content, delay = 0, trigger = 'hov
   tooltip.innerHTML = content;
   tooltip.style.backgroundColor = backgroundColor;
   tooltip.style.color = color;
+
+  tooltip.style.setProperty('--tooltip-bg-color', backgroundColor);
 
   element.style.position = 'relative';
   element.appendChild(tooltip);
